3de6a08f9ffb3e2a39266b4a6ac9829120d4d039
nazeehsalah <nazeehsalah28@gmail.com>
2018-07-23 11:20:13 +0200

    commit0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ import { UserProfilePage } from '../pages/user-profile/user-profile';
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav
-  rootPage: any = LogInPage;
+  rootPage: any = CalenderEventsPage;
   pages: Array<{ title: string, component: any, icon: string }>;
 
   constructor(
